8dc28c1df1a1f210fccf657eb9990e7e0439a016
Rohit Dhumal <Rohit@Rohits-MacBook-Air.local>
2017-10-30 21:46:01 -0700

    Added security types

---
+++ garderie-types/src/main/java/com/garderie/types/security/consts/SecurityConsts.java
@@ -0,0 +1,10 @@
+package com.garderie.types.security.consts;
+
+public final class SecurityConsts {
+
+    public static final String AUTH_HEADER_NAME = "x-auth-token";
+
+    private SecurityConsts() {
+
+    }
+}
